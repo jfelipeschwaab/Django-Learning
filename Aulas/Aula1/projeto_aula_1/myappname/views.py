37aa81d6d70972ec4de0eb5d129e7fcfd6e26a43
João Felipe <joaofsp.games@gmail.com>
2024-07-16 11:53:58 -0300

    Updating project-aula-1

--- Aulas/Aula1/projeto_aula_1/myappname/views.py
+++ Aulas/Aula1/projeto_aula_1/myappname/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.template import loader
 
 # Create your views here.
+def home(request):
+    template = loader.get_template("app/home.html")
+    return render(request, template)
+
+
+def profile(request):
+    pass
